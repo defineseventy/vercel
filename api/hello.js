1266812056bcc7783d6521d1dddd6fa356661dbc
P1935404 <63201020+P1935404@users.noreply.github.com>
2023-06-11 22:48:49 +0800

    added index.js

--- api/hello.js
+++
@@ -1,3 +0,0 @@
-export default function hello(req, res) {
-    res.status(200).send("'Result':'Succcess'");
-}   
---
+++ index.js
@@ -0,0 +1,3 @@
+export default function index(req, res) {
+    res.send(200).message({"Results": "Success"})
+}
