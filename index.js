b5f73957161724d0317415741b97d56bfd7ec7dc
P1935404 <63201020+P1935404@users.noreply.github.com>
2023-06-11 23:08:13 +0800

    only leave the test part

--- index.js
+++ index.js
@@ -14,19 +14,4 @@ app.get("/api/test", (req, res) => {
   res.send("test");
 });
 
-// app.use(express.static(path.join(__dirname, "./frontend/build")));
-
-// app.get("*", function (_, res) {
-//   res.sendFile(
-//     path.join(__dirname, "./frontend/build/index.html"),
-//     function (err) {
-//       if (err) {
-//         res.status(500).send(err);
-//       }
-    // });
-// });
-
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server Running on port ${port}`));
-
 module.exports = app;
